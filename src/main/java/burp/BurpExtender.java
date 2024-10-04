617df910042935f604e0142794c5f1f435266b42
Hannah Law <hannah.law@portswigger.net>
2024-10-04 11:42:00 +0100

    Use Extender output mechanism for error logging.

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,16 +1,12 @@
 package burp;
 
+@SuppressWarnings("unused")
 public class BurpExtender implements IBurpExtender
 {
-
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
     {
-        IExtensionHelpers helpers = callbacks.getHelpers();
-
         callbacks.setExtensionName("Content-Type Converter");
-
-        callbacks.registerContextMenuFactory(new Menu(helpers));
-
+        callbacks.registerContextMenuFactory(new Menu(callbacks));
     }
 }
--- src/main/java/burp/Menu.java
+++ src/main/java/burp/Menu.java
@@ -3,20 +3,27 @@ package burp;
 import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Menu implements IContextMenuFactory {
-    private final IExtensionHelpers m_helpers;
+import static burp.IBurpExtenderCallbacks.TOOL_INTRUDER;
+import static burp.IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST;
 
-    public Menu(IExtensionHelpers helpers) {
-        m_helpers = helpers;
+public class Menu implements IContextMenuFactory {
+    private final IBurpExtenderCallbacks callbacks;
+    private final IExtensionHelpers helpers;
+
+    public Menu(IBurpExtenderCallbacks callbacks) {
+        this.callbacks = callbacks;
+        this.helpers = callbacks.getHelpers();
     }
 
     public List<JMenuItem> createMenuItems(final IContextMenuInvocation invocation) {
-        List<JMenuItem> menus = new ArrayList();
+        List<JMenuItem> menus = new ArrayList<>();
 
-        if (invocation.getToolFlag() != IBurpExtenderCallbacks.TOOL_INTRUDER && invocation.getInvocationContext() != IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST){
+        if (invocation.getToolFlag() != TOOL_INTRUDER && invocation.getInvocationContext() != CONTEXT_MESSAGE_EDITOR_REQUEST){
             return menus;
         }
 
@@ -45,13 +52,15 @@ public class Menu implements IContextMenuFactory {
             public void mouseReleased(MouseEvent arg0) {
                 IHttpRequestResponse iReqResp = invocation.getSelectedMessages()[0];
                 try {
-                    byte[] request = Utilities.convertToXML(m_helpers, iReqResp);
+                    byte[] request = Utilities.convertToXML(helpers, iReqResp);
                     if (request != null) {
 
                         iReqResp.setRequest(request);
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    StringWriter out = new StringWriter();
+                    e.printStackTrace(new PrintWriter(out));
+                    callbacks.printError(out.toString());
                 }
             }
         });
@@ -79,21 +88,21 @@ public class Menu implements IContextMenuFactory {
             public void mouseReleased(MouseEvent arg0) {
                 IHttpRequestResponse iReqResp = invocation.getSelectedMessages()[0];
                 try {
-                    byte[] request = Utilities.convertToJSON(m_helpers, iReqResp);
+                    byte[] request = Utilities.convertToJSON(helpers, iReqResp);
                     if (request != null) {
 
                         iReqResp.setRequest(request);
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    StringWriter out = new StringWriter();
+                    e.printStackTrace(new PrintWriter(out));
+                    callbacks.printError(out.toString());
                 }
             }
         });
-
 
         menus.add(sendXMLToRepeater);
         menus.add(sendJSONToRepeater);
         return menus;
     }
-
 }
